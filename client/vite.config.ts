5bb108d30262e806f85d90598e7f56bb50b96761
Pragesh Pant <pantprageshp1@gmail.com>
2026-02-13 22:13:29 +0545

    Update route paths

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,4 +13,16 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, "src/components"),
+      pages: path.resolve(__dirname, "src/pages"),
+      utils: path.resolve(__dirname, "src/utils"),
+      hook: path.resolve(__dirname, "src/hook"),
+      contexts: path.resolve(__dirname, "src/contexts"),
+      layout: path.resolve(__dirname, "src/layout"),
+      constant: path.resolve(__dirname, "src/constant"),
+      api: path.resolve(__dirname, "src/api"),
+    },
+  },
 });
